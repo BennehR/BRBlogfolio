dbe0d2cc90e667a449f7a381bcc0c67b300199be
BennehR <ben.c.rendall@gmail.com>
2017-06-11 12:24:52 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,107 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>BR Blogfolio</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/custom.css" type="text/css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+    <?php include ("navbar.php"); ?>
+
+    <div class="container-fluid" id="breadcrumbs">
+        <div class="row">
+            <div class="col-sm-12">
+                <ul class="breadcrumb">
+                    <li>You are here</li>
+                    <li><a href="#">Page 1</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid topBuffer" id="bannerTop">
+        <div class="row">
+            <div class="col-sm-1">
+            </div>
+            <div class="col-sm-10">
+                <div class="jumbotron bannerImg topBuffer">
+                    <img src="css/images/logo.png" class="jumboLogo"/>
+                </div>
+            </div>
+            <div class="col-sm-1">
+            </div>
+        </div>
+        
+        <div class="row topBuffer" id="contentMain">
+            <div class="col-sm-1">
+            </div>
+            <div class="col-sm-10 boxContent">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="topBuffer botBuffer" style="position:relative;height:0;padding-bottom:56.25%">
+                            <iframe src="https://www.youtube.com/embed/oO79MZ4dgWA?ecver=2" width="640" height="360" frameborder="0" style="position:absolute;width:100%;height:100%;left:0" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <h1>Header</h1>
+                        <p>Text goes here <br /></p>
+                        <p>Text goes here <br /></p>
+                        <p>Text goes here <br /></p>
+                        <p>Text goes here <br /></p>
+                        <p>Text goes here <br /></p>
+                        <p>Text goes here <br /></p>
+                        <p>Text goes here <br /></p>
+                        <p>Text goes here <br /></p>
+                        <p>Text goes here <br /></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-1">
+            </div>
+        </div>
+        <div class="row topBuffer">
+            <div class="col-sm-1">
+            </div>
+            <div class="col-sm-10 boxContent">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <h1>Header</h1>
+                        <p>Text goes here <br /></p>
+                        <p>Text goes here <br /></p>
+                        <p>Text goes here <br /></p>
+                        <p>Text goes here <br /></p>
+                        <p>Text goes here <br /></p>
+                        <p>Text goes here <br /></p>
+                        <p>Text goes here <br /></p>
+                        <p>Text goes here <br /></p>
+                        <p>Text goes here <br /></p>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="topBuffer botBuffer" style="position:relative;height:0;padding-bottom:56.25%">
+                            <iframe src="https://www.youtube.com/embed/oO79MZ4dgWA?ecver=2" width="640" height="360" frameborder="0" style="position:absolute;width:100%;height:100%;left:0" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-1">
+            </div>
+        </div>
+        <div class="row topBuffer">
+            <div class="col-sm-1">
+            </div>
+            <div class="col-sm-10 boxContent">
+                <h1>Header!</h1>
+                <p> t <br />t <br />t <br />t <br />t <br />t <br />t <br /></p>
+            </div>
+            <div class="col-sm-1">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,47 @@
+<div class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#"><img src="css/images/logo.jpg" class="logo"/></a>
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavBar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+
+            <div class="collapse navbar-collapse" id="myNavBar">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                            3D Work
+                            <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Page 1</a></li>
+                            <li><a href="#">Page 2</a></li>
+                            <li><a href="#">Page 3</a></li>
+                            <li><a href="#">Page 4</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                            Coding Work
+                            <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Page 1</a></li>
+                            <li><a href="#">Page 2</a></li>
+                            <li><a href="#">Page 3</a></li>
+                            <li><a href="#">Page 4</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Projects</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">About</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
