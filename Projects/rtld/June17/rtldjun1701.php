7305943ecb3f3c9a75fe4fa3a221ace8a95fa7ab
Ben.Rendall <Ben.Rendall@centre1.uk.boots.com>
2017-06-12 13:09:21 +0100

    Filled out rtldjun1701 and added pagination at the bottom for navigation

--- Projects/rtld/June17/rtldjun1701.php
+++ Projects/rtld/June17/rtldjun1701.php
@@ -35,6 +35,7 @@
                     <li><a href="/index.php">Home</a></li>
                     <li><a href="/Projects/projects.php">Projects</a></li>
                     <li><a href="/Projects/rtld.php">Road To Ludum Dare</a><li>
+                    <li>June 2017 - Page 1</li>
                 </ul>
             </div>
         </div>
@@ -44,9 +45,21 @@
         <div class="row">
             <div class="col-sm-1"></div>
             <div class="col-sm-10 boxContent topBuffer">
-                <h1>Contents</h1>
-                <p>Select a page to view
-                <br/>This contents page will be periodically updated with new content, each page will have navigation buttons at the bottom to go forward or backward in time through the updates.</p>
+                <h1>The Concept</h1>
+                <p>
+                Ludum dare has always been somthing that has interested me. I took a diploma in college for Games Design as I've always had a passion and interest in that field.
+                Coupled with a university degree in 3D Animation & Modelling I have the right base skills for this kind of thing, but theres a catch.
+                <br />
+                I've never been great at coding, its been an interest but somthing that's always been brushed off as somthing I cant do, or too complicated to learn. 
+                The second issue is after finishing University my student license for 3DS Max ran out, and all access to the software is lost.
+                Having spent 5+ years using 3DS Max I've been reluctant to learn anything else due to the learning curve and habits that are already ingrained from my course and time in the software.
+                <br />
+                Recently I got myself Blender, somthing I have used in the past in college but never really got to grips with completely due to lack of training and/or interest at the time.
+                I started to watch some tutorials, read some articles and decided to give it ago and started taking part in modelling for a twitter daily event <a href="https://twitter.com/search?q=%23lowpoly_dailies&src=tyah&lang=en">#LowPoly_Dailies</a> where you model somthing, in low poly about a given topic each day.
+                <br />
+                It only took two of these models to relight a passion in me for 3D and I decided with my new skills in coding I can take on Ludum Dare, but not just yet.
+                </p>
+                <p class="textCenter">This is my road to take part in Ludum Dare</p>
             </div>
             <div class="col-sm-1"></div>
         </div>
@@ -54,14 +67,15 @@
         <div class="row">
             <div class="col-sm-1"></div>
             <div class="col-sm-10 boxContent topBuffer">
-                <ul>
-                    <li><a href="/June17/rtldjun1701.php">June 2017</a></li>
-                    <li>More to come...</li>
+                <ul class="pager">
+                    <li class="previous disabled">&larr; Older</li>
+                    <li><a href="/Projects/rtld.php">Contents</a></li>
+                    <li class="next"><a href="#">Newer &rarr;</a></li>
                 </ul>
             </div>
             <div class="col-sm-1"></div>
         </div>
     </div>
-     
+    
 </body>
 </html>
