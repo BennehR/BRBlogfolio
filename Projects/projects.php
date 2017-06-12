f0c873277de99ad2d250a586c98cc8329b5c4ed6
Ben.Rendall <Ben.Rendall@centre1.uk.boots.com>
2017-06-12 08:27:08 +0100

    Updated projects page with section on the website

--- Projects/projects.php
+++ Projects/projects.php
@@ -62,6 +62,26 @@
             </div>
             <div class="col-sm-1"></div>
         </div>
+
+        <div class="row topBuffer">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-10 boxContent">
+                <h1 class="textCenter">BR Blogfolio</h1>
+                <p>
+                This whole website is a project! In the past I have used things like WordPress and they can provide a great result, but I wanted to create my own website for a couple reasons. <br/>
+                <ul>
+                    <li>Skill development - Creating everything myself (within reason) gives me a chance to develop new skills in HTML, CSS, JS, jQuery, PHP and general front end web development.</li>
+                    <li>Project to work on - I'm always looking for somthing to keep me busy. Between projects at work I can get bored and tend to find reasons to code or create somthing, with this I have a constant project to work on over time.</li>
+                    <li>Full customisation - I can customise the site how i want it.</li>
+                    <li>Sandbox - I now have a web environment to play with, opening doors not just for my new web dev skills but my C#.NET skills too as I can start to explore ASP.NET.</li>
+                </ul>
+                <br />There are two parts to the site, live and dev. The live side of the website is probably what your seeing now, the development side is where I will be testing any changes to the site before rolling them out to the live environment.
+                This is how its done in the professional world, so I decided to replicate it with a sub-domain.
+                <br />You can see the dev environemnt (although there may not be any difference most of the time) at <a href="dev.br.x10host.com">dev.br.x10host.com</a>.
+                </p>
+            </div>
+            <div class="col-sm-1"></div>
+        </div>
     </div>
      
 </body>
